54b93d613fdf40c000d114f06e9887dd56d578ca
David <davidjorrin88@gmail.com>
2024-11-14 16:19:23 -0300

    cambios en proximidad random forest en r

--- src/Competencia_2/proximidad_random_forest.r
+++ src/Competencia_2/proximidad_random_forest.r
@@ -15,11 +15,16 @@ dataset <- read.csv(gzfile("competencia_02.csv.gz"))
 # Sample dataset
 set.seed(123)
 
-data_08 <- dataset  # Using the first 4 columns of the iris dataset as an example
+data_08 <- dataset[dataset$foto_mes == "202108", ]  # Using the first 4 columns of the iris dataset as an example
 target <- iris$Species
 
 # New vector to calculate proximity with
 new_vector <- data.frame(Sepal.Length = 5.1, Sepal.Width = 3.5, Petal.Length = 1.4, Petal.Width = 0.2)
+
+
+# Entrenar el modelo Random Forest en modo no supervisado con proximidad activada
+set.seed(123)
+rf_model <- randomForest(data, proximity = TRUE, ntree = 500)
 
 # Append the new vector to the original dataset
 data_combined <- rbind(data, new_vector)
